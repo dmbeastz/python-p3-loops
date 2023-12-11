cd30b13abb65ec619fe6f4f82c020cf427ec8262
dmbeastz <daveroymutisya2@gmail.com>
2023-12-11 12:41:38 +0300

    Completed Lab

--- lib/looping.py
+++ lib/looping.py
@@ -21,5 +21,18 @@ print(result)
 
 
 def fizzbuzz():
-    
-    pass
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+
+fizzbuzz()
+
+
+
